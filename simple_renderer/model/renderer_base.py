9b611cefa653046b2573165401405fab1baf1c7a
makintosz <grzegorz.kozinski34@gmail.com>
2022-05-28 21:26:49 +0200

    basic working model added

---
+++ simple_renderer/model/renderer_base.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+
+class SceneRenderer(ABC):
+    @abstractmethod
+    def train(self, x: np.ndarray, y: np.ndarray) -> dict[str, list]:
+        pass
+
+    @abstractmethod
+    def generate_all_frames(self, epoch: int) -> None:
+        pass
+
+    @abstractmethod
+    def generate_frame(self, data: list) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def save_model(self) -> None:
+        pass
+
+    @abstractmethod
+    def load_model(self, path_linear: str, path_convolutional: str) -> None:
+        pass
